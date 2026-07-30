686200e7699451ca1f047967ea017c512147c33e
sieni7 <sieni7@gmail.com>
2026-07-30 21:32:58 +0000

    fix: unregister old service workers to fix MIME type error

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((regs) => {
+    regs.forEach((reg) => reg.unregister())
+  })
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
